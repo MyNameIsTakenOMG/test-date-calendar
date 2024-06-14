0b2755ccd8eccc3de4023eb24e9a502b0a462a40
Sean Fang <fangzhengonly@gmail.com>
2024-06-14 09:59:26 -0400

    add lazy loading--next/dynamic

--- src/app/booking/page.tsx
+++ src/app/booking/page.tsx
@@ -1,12 +1,28 @@
 'use client'
-import React from 'react'
+import React, { useState } from 'react'
 import { LocalizationProvider } from '@mui/x-date-pickers';
 import { AdapterDayjs } from '@mui/x-date-pickers/AdapterDayjs'
-import TestCalendar from '@/components/TestCalendar';
+// import TestCalendar from '@/components/TestCalendar';
+
+import dynamic from 'next/dynamic';
+
+const ClientCalendar1 = dynamic(() => import('@/components/TestCalendar'), { ssr: false })
+
+const ClientCalendar2 = dynamic(() => import('@/components/TestCalendar'))
+
 export default function Booking() {
+
+  const [show, setShow] = useState(false)
+
   return (
     <LocalizationProvider dateAdapter={AdapterDayjs}>
-      <TestCalendar />
+
+      {/* only load at the client side */}
+      <ClientCalendar1 />
+
+      {show && <ClientCalendar2 />}
+      <button onClick={() => setShow(!show)}>toggle</button>
+
     </LocalizationProvider>
   )
 }
